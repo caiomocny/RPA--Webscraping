34e6971e9b635dc448539288ae8d5acf4d20bcbd
Caio Mocny <91752440+caiomocny@users.noreply.github.com>
2023-04-07 18:07:52 -0300

    Update Automação Web e Busca de Informações com Python.py

--- Automação Web e Busca de Informações com Python.py
+++ Automação Web e Busca de Informações com Python.py
@@ -1,31 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Automação Web e Busca de Informações com Python
-
-# #### Desafio: 
-# Trabalhamos em uma importadora e o preço dos nossos produtos é vinculado a cotação de:
-# - Dólar
-# - Euro
-# - Ouro
-# Precisamos pegar na internet, de forma automática, a cotação desses 3 itens e saber quanto devemos cobrar pelos nossos produtos, considerando uma margem de contribuição que temos na nossa base de dados.
-# Base de Dados: https://drive.google.com/drive/folders/1KmAdo593nD8J9QBaZxPOG1yxHZua4Rtv?usp=sharing
-# Para isso, vamos criar uma automação web:
-# - Usaremos o selenium
-# - Importante: baixar o webdriver
-
-# webdrivers: Chrome -> chromedriver / Firefox -> geckodriver
-
-
 #Passo 1 e 2: acessar cotação na internet e armazenar a informação
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 navegador = webdriver.Chrome() # selenium está instalado no diretório do Python da máquina.
-#         = webdriver.Chrome("chromedriver.exe") -> caso o selenium esteja instalado apenas no diretório do projeto.
 
-# Pegar cotação dolar
+# Elemento cotação dolar
 navegador.get('https://www.google.com.br/')
 navegador.find_element(By.XPATH,
                        '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação dolar')
@@ -35,7 +15,7 @@ dolar = float(navegador.find_element(By.XPATH,
                         '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')) #tornar a informação em valor númerico flutuante (float)
 print(f'{dolar:.2f}', type(dolar))
 
-# Pegar cotação euro
+# Elemento cotação euro
 navegador.get('https://www.google.com.br/')
 navegador.find_element(By.XPATH, 
                        '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação euro')
@@ -45,7 +25,7 @@ euro = float(navegador.find_element(By.XPATH,
                         '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')) #tornar a informação em valor númerico flutuante (float)
 print(f'{euro:.2f}', type(euro))
 
-# Pegar cotação do ouro
+# Elemento cotação do ouro
 navegador.get('https://www.melhorcambio.com/ouro-hoje')
 ouro = navegador.find_element(By.XPATH, 
                         '//*[@id="comercial"]').get_attribute('value')
@@ -54,10 +34,7 @@ print(f'{ouro:.2f}', type(ouro))
 
 navegador.quit() #fechar o navegador
 
-
-# ### Agora vamos atualizar a nossa base de preços com as novas cotações
-
-# - Importando a base de dados
+# Importando a base de dados
 
 #Passo 3:  importar banco de dados para a pasta de trabalho
 import pandas as pd
@@ -72,7 +49,7 @@ df.loc[df['Moeda'] == 'Ouro', 'Cotação'] = ouro
 display(df)
 
 
-# - Atualizando os preços e o cálculo do Preço Final
+# Atualizando os preços e o cálculo do Preço Final
 
 #Passo 5: atualizar banco de dados (demais colunas)
 #Coluna Preço de Compra
@@ -84,6 +61,6 @@ df['Preço de Venda'] = df['Preço de Compra']*df['Margem']
 display(df)
 
 
-# ### Agora vamos exportar a nova base de preços atualizada
+# Agora vamos exportar a nova base de preços atualizada
 
 df.to_excel('Novos Produtos.xlsx', index = False)
